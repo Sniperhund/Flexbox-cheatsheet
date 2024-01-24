6a891f5dc352608e9c1c05c1c67ceddd37aacb90
Lucas Täkker <lucas@lucasskt.dk>
2024-01-24 19:01:10 +0100

    Done mostly

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("view");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.classList.add("better-view");
+    } else {
+        text.classList.remove("better-view");
+    }
+}
